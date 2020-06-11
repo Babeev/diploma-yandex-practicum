93c290338343b8eec5095bf354b329aaf26f3e06
Babeev <babeev.nikita@gmail.com>
2020-06-11 12:56:59 +0700

    fix package settings

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -30,6 +30,14 @@ plugins: [
         template: './src/index.html',
         filename: 'index.html'
       }),
-    new WebpackMd5Hash()
+    new WebpackMd5Hash(),
+    new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.optimize\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorPluginOptions: {
+          preset: ['default', { discardComments: { removeAll: true } }],
+        },
+        canPrint: true
+      })
 ]
 };
